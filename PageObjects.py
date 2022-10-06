b58b2485a35ee696285ef24345a0a9cb76c76fb8
Destroymon <110887215+Destroymon@users.noreply.github.com>
2022-10-06 19:43:41 +0300

    ычыч

--- PageObjects.py
+++ PageObjects.py
@@ -1,4 +1,6 @@
-
+from random import random
+import time
+from selenium.webdriver import Keys, ActionChains
 from BaseApp import BasePage
 from selenium.webdriver.common.by import By
 
@@ -9,11 +11,6 @@ from selenium.webdriver.common.by import By
 class ChromeLocators:
     LOCATOR_CHOISE_MOSCOW = (By.CSS_SELECTOR, "a.locality-selector-popup__big-city")
 
-    # find_elements_by_class_name = (
-    #     By.XPATH, "//div[@class='sc-1tpn8pe-3 duQqqx']")
-    #
-    # LOCATOR_lENGTH_PIZZA = len(find_elements_by_class_name)
-
     LOCATOR_CHOISE_MOSCOW_Header = (By.CSS_SELECTOR,
                                     "header.sc-1of5u0p-0.hpZTJI "
                                     "div.left div.yrfxdq-0.jjxTjA.header__about h1.header__about-slogan:nth-child(1) > "
@@ -21,9 +18,10 @@ class ChromeLocators:
                                     "-slogan "
                                     "-text_link:nth-child(2))")
 
-    # LOCATOR_lENGTH_PIZZA = (By.CLASS_NAME, "sc-1tpn8pe-3 duQqqx")
     LOCATOR_lENGTH_PIZZA = (By.CLASS_NAME, "sc-1tpn8pe-3 duQqqx")
     LOCATOR_WINDOW_PIZZA = (By.XPATH, "//body/div[@id='react-app']/main[1]/section[1]/article[1]")
+    LOCATOR_SIZE_SMALL_PIZZA = (By.XPATH, "//label[contains(text(),'Маленькая')]")
+    LOCATOR_ADD_TO_CARD = (By.XPATH, "//body/div[4]/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/button[1]")
 
 class DODORegion(BasePage):
     # В click_on_region_button мы нажимаем на кнопку "Москва" в выборе региона доставки.
@@ -36,7 +34,7 @@ class LenghtPizza(BasePage):
     def look_for_element(self):
         self.driver.find_element(ChromeLocators.LOCATOR_lENGTH_PIZZA)
         return self.driver.find_element(ChromeLocators.LOCATOR_lENGTH_PIZZA)
-        
+
 
 
 class RandomPizza(BasePage):
@@ -44,5 +42,54 @@ class RandomPizza(BasePage):
         self.driverwait(ChromeLocators.LOCATOR_CHOISE_MOSCOW).click()
     def click_random_pizza(self):
         self.driverwait(ChromeLocators.LOCATOR_WINDOW_PIZZA).click()
-        # l = links[randint(0, len(links) - 1)]
-        # l.click()
+    def click_small_pizza(self):
+        self.driverwait(ChromeLocators.LOCATOR_SIZE_SMALL_PIZZA).click()
+    def click_add_to_card(self):
+        self.driverwait(ChromeLocators.LOCATOR_ADD_TO_CARD).click()
+
+def func_five(browser):    #Функция для выбора 5 пицц
+    actions = ActionChains(browser)
+    test = 0
+    elements = browser.find_elements(By.XPATH, "//section[@id='pizzas']//article")
+    for i in range(5):
+        actions.send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        test = len(elements)
+    add = []
+    for i in elements:
+        add.append(i)
+    random_index = random.randrange(len(add))
+    if random_index == 3:
+        click = browser.find_element(By.XPATH,
+                                     "//article[@data-yellow='true']//button[@type='button'][contains(text(),'589₽')]")
+        return click, test
+    elif random_index == 2:
+        click = browser.find_element(By.XPATH,
+                                     "//body[1]/div[3]/main[1]/section[1]/article[2]/div[1]/button[1]")
+        return click, test
+    elif random_index == 1:
+        click = browser.find_element(By.XPATH,
+                                     "//button[contains(text(),'Собрать')]")
+        return click, test
+    elif random_index > 3:
+        click = browser.find_element(By.XPATH,
+                                     f'//body[1]/div[3]/main[1]/section[1]/article[{random_index}]/footer[1]/button')
+        print(random_index)
+        return click, test
+
+def func_one(browser):         # Функция для вызбора 1 пиццы
+    actions = ActionChains(browser)
+    test = 0
+    elements = browser.find_elements(By.XPATH, "//section[@id='pizzas']//article")
+    for i in range(5):
+        actions.send_keys(Keys.PAGE_DOWN).perform()
+        time.sleep(1)
+        test = len(elements)
+    add = []
+    for i in elements:
+        add.append(i)
+    random_index = random.randrange(len(add))
+    click = browser.find_element(By.XPATH,
+                                 f'//body[1]/div[3]/main[1]/section[1]/article[{random_index}]/footer[1]/button')
+    browser.execute_script("arguments[0].click();", click)
+    return test
--- Test_pizza.py
+++ Test_pizza.py
@@ -1,6 +1,10 @@
+import random
+import time
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-from PageObjects import DODORegion, LenghtPizza, RandomPizza
+from PageObjects import DODORegion, LenghtPizza, RandomPizza, func_five
 
 
 # Создаем класс для первого кейса проверок
@@ -19,13 +23,23 @@ class Test_Case_0:
                                                                 "_about-slogan-text_link:nth-child(2)")  #Считываем текст
         assert header_lolation.text == "Москва"     # Проверяем, что тест соответсвует ОР
 
-    def test_quantity_pizza_moscow_page(self, browser):
+    def test_quantity_pizza_moscow_page(self, browser):          #Кол-во Пицц на странице(спойлер, тест падает потому что не сходится с ОР = 34 :))
         quantity = LenghtPizza(browser)
         quantity.go_to_site()
         quantity.click_on_region_button()
-        quantity.look_for_element()
-
-        assert len(quantity.look_for_element()) == 1
+        actions = ActionChains(browser)
+        test = 0
+        elements = browser.find_elements(By.XPATH, "//section[@id='pizzas']//article")
+        for i in range(5):
+            actions.send_keys(Keys.PAGE_DOWN).perform()
+            time.sleep(1)
+            test = len(elements)
+        add = []
+        for i in elements:
+            add.append(i)
+        random_index = random.randrange(len(add))
+        add[random_index].click()
+        assert test == 34
 
 
 class Test_Case_1:
@@ -36,4 +50,80 @@ class Test_Case_1:
         random.click_random_pizza()
         url = browser.current_url
         assert url == 'https://dodopizza.ru/moscow/pizza/halfs'     #Проверяем что открывается окно конструктора пицц
+
+    def test_add_random_pizza_to_cart(self, browser):  # Проверить, что в шапке главной страницы, в кнопке "Корзина"
+                                                       # появился счётчик добавленных товаров. Значение = 1.
+        quantity = RandomPizza(browser)
+        quantity.go_to_site()
+        quantity.click_on_region_button()
+        actions = ActionChains(browser)
+        test = 0
+        elements = browser.find_elements(By.XPATH, "//section[@id='pizzas']//article")
+        for i in range(5):
+            actions.send_keys(Keys.PAGE_DOWN).perform()
+            time.sleep(1)
+            test = len(elements)
+        add = []
+        for i in elements:
+            add.append(i)
+        random_index = random.randrange(len(add))
+        click = browser.find_element(By.XPATH,
+                                     f'/html[1]/body[1]/div[3]/main[1]/section[1]/article[{random_index}]/main[1]/div[1]')
+
+        # Переменную клик нужно изменить, выбрать элемент случайный с кодом f'//body[1]/div[3]/main[1]/section[1]/article[{random_index}]/footer[1]/button') это кнопка "Выбрать"
+        # Однако, как сделать две проверки чтобы они работали, чтобы проверить и название и переход по щелчку на кнопку "выбрать" я не знаю, я пробовал разные методы, возможно нужно написать еще оджну функцию, чтобы выборать
+        # Элемент который выбирается и при рандомном нажатии в функции сверху, но это займет долгое время для решения у меня, в сроки сдачи не уложусь
+
+        browser.execute_script("arguments[0].click();", click)
+        time.sleep(5)
+        name_pizza_windows = browser.find_element(By.CSS_SELECTOR, "div.sc-1dazsw3-3.dfOCuw.show div.sc-1dazsw3-2.pnLpo" 
+                                                                   " div.sc-1dazsw3-1.dHDRyZ div.gsrbdo-0.bFKozP div.gs"
+                                                                   "rbdo-8.dQwDFJ div.gsrbdo-9.eDBIfW div.sc-11ezv7x-0.hA"
+                                                                   "-DLvj div:nth-child(1) div:nth-child(1) div.gsrbdo-"
+                                                                   "10.emwqeU div.gsrbdo-11.dtJPrk > span.gsrbdo-12.hshULL")
+        # Проверка того
+        assert name_pizza_windows.text == click.text
+        quantity.click_add_to_card()
+        time.sleep(5)
+        card_qantity = browser.find_element(By.XPATH, '//*[@id="react-app"]/nav/div/div[2]/button/div[2]')
+        assert card_qantity.text == "1"  # Проверка, что в корзине одна пицца
+        time.sleep(10)
+        return test
+
+    def test_add_differets_cost(self, browser):  # Проверить, что в шапке главной страницы, в кнопке "Корзина"
+        # появился счётчик добавленных товаров. Значение = 1.
+        quantity = RandomPizza(browser)
+        quantity.go_to_site()
+        quantity.click_on_region_button()
+        actions = ActionChains(browser)
+        test = 0
+        elements = browser.find_elements(By.XPATH, "//section[@id='pizzas']//article")
+        for i in range(5):
+            actions.send_keys(Keys.PAGE_DOWN).perform()
+            time.sleep(1)
+            test = len(elements)
+        add = []
+        for i in elements:
+            add.append(i)
+        random_index = random.randrange(len(add))
+        cost = browser.find_element(By.XPATH,
+                                     f'/html[1]/body[1]/div[3]/main[1]/section[1]/article[{random_index}]/footer[1]/div[1]')
+
+        #cost_prace = browser.find_element(By.XPATH, f'/html/body/div[3]/main/section[1]/article[{random_index}]/footer/div/text()[2]')
+        browser.execute_script("arguments[0].click();", cost)
+        time.sleep(5)
+        name_pizza_windows = browser.find_element(By.CSS_SELECTOR, "div.sc-1dazsw3-3.dfOCuw.show div.sc-1dazsw3-2.pnLpo div.sc-1dazsw3-1.dHDRyZ div.gsrbdo-0.bFKozP div.gsrbdo-8.dQwDFJ div.gsrbdo-18.dlfBaI button.sc-1rmt3mq-0.cpUbDl.gsrbdo-22.gaQAWN span.money > span.money__value")
+
+        # Проверка, что цены отличаются
+        assert name_pizza_windows.text != cost.text     # Проверка, что значение цены на кнопке "Добавить в корзину за
+        # {}Р" изменится. Сравнить со значением на главной странице (т.к. там указана цена за мелнькую пиццу)
+
+class Test_Case_2:
+    def test_quantity_pizza_moscow_page(self, browser):
+        quantity = LenghtPizza(browser)
+        quantity.go_to_site()
+        quantity.click_on_region_button()
+        arguments = func_five(browser)
+        browser.execute_script("arguments[0].click();", arguments[0])
+        assert arguments[1] == 34
 
